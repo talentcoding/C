96e4cc4d9415934c00b9707aa8047f63e71c9339
santhosh mari <talentcodings@gmail.com>
2026-01-03 10:04:01 +0530

    Add one.c to Strings/strLength directory

---
+++ Strings/strLength/one.c
@@ -0,0 +1,13 @@
+// string length using strlib function
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	char str[20]="coding";
+	int x = strlen(str);
+	printf("string length =%d \n",x);
+}
