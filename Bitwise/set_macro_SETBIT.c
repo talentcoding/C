25d3483c35c2953e1832f9679421e463b903697f
talentcoding <talentcodings@gmail.com>
2026-01-07 16:23:43 +0530

    setting the bit using macro SETBIT using bitwise operator

---
+++ Bitwise/set_macro_SETBIT.c
@@ -0,0 +1,33 @@
+// setting a bit using macro 
+
+#include <stdio.h>
+
+#define SET_BIT(num,pos)  num|=(0x01<<pos)
+
+void print_bits(unsigned int num);
+
+int main()
+{
+	unsigned int num,pos;
+	printf("enter a number and position\n");
+	scanf("%d%d",&num,&pos);
+
+	print_bits(num);
+
+	SET_BIT(num,pos);
+
+	print_bits(num);
+
+}
+
+void print_bits(unsigned int num)
+{
+        char i=31;
+
+        for(i=31;i>=0;i--)
+                printf("%d",(num>>i)&0x01);
+
+        printf("\n---END---\n");
+
+}
+
